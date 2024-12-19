dc3adc4d2e9d4f22d2b6c2acf6de072e8ff374ba
PES <AMM@DESKTOP-PIOCC7V>
2024-12-19 10:55:11 +0630

    update

--- DTOs/TMS_ProposalDto.cs
+++ DTOs/TMS_ProposalDto.cs
@@ -24,6 +24,9 @@ namespace TMS_Api.DTOs
         public string? CreatedUser { get; set; }
         public string? Customer { get; set; }
         public string? InYardID { get; set; }
+        public string? PCCode { get; set; }
+        public string? AreaID { get; set; }
+        public string? BLNo { get; set; }
         public List<TMS_ProposalDetailDto>? DetailList { get; set; }
         public DataTable? ProposalDetailList { get; set; }
         
